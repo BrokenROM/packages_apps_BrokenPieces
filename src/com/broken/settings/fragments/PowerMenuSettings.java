8286d2571e63a74669c6de166570eb88f3293e12
Lars Greiss <kufikugel@googlemail.com>
2015-10-23 21:51:34 -0500

    Add reboot menu [2/2]

--- src/com/broken/settings/fragments/PowerMenuSettings.java
+++ src/com/broken/settings/fragments/PowerMenuSettings.java
@@ -16,26 +16,64 @@
 
 package com.broken.settings.fragments;
 
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class PowerMenuSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+    private static final String KEY_ADVANCED_REBOOT = "advanced_reboot";
+
+    private ListPreference mAdvancedReboot;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.xml.powermenu_settings);
-        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mAdvancedReboot = (ListPreference) findPreference(KEY_ADVANCED_REBOOT);
+        mAdvancedReboot.setValue(String.valueOf(Settings.Secure.getInt(
+                getContentResolver(), Settings.Secure.ADVANCED_REBOOT, 0)));
+        mAdvancedReboot.setSummary(mAdvancedReboot.getEntry());
+        mAdvancedReboot.setOnPreferenceChangeListener(this);
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.BROKEN;
     }
-}
 
+    @Override
+    public void onResume() {
+		super.onResume();
+	}
+
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        if (preference == mAdvancedReboot) {
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.ADVANCED_REBOOT,
+                    Integer.valueOf((String) value));
+            mAdvancedReboot.setValue(String.valueOf(value));
+            mAdvancedReboot.setSummary(mAdvancedReboot.getEntry());
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+}
